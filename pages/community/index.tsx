f4ffe439621109edb9d445aea26ebccfad1d4e5e
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-02-21 15:21:12 +0000

    Community Detail

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -4,7 +4,7 @@ const Community: NextPage = () => {
   return (
     <div className="py-16 px-4 space-y-8">
      {[1,2,3,4,5,6].map((_, i )=> <div key={i} className="flex cursor-pointer flex-col items-start">
-        <span className="flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">동네질문</span>
+        <span className="flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">동네질문</span>
         <div className="mt-2 text-gray-700">
           <span className="text-orange-500 font-medium">Q.</span> What is the best mandu restaurant?
         </div>
