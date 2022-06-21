96fb31c71415d819faa779531844db693b252a70
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-21 12:57:47 +0100

    reach hook register function

---
+++ pages/forms.tsx
@@ -0,0 +1,22 @@
+import { useForm } from "react-hook-form";
+
+// Less code
+// Better validation
+// Better errors(set, clear, display   )
+// Have control over inputs
+// Dont deal with events
+//  Easier Inputs
+
+export default function Forms() {
+    const {register, watch} = useForm();
+    
+    return(
+        <form >
+            <input {...register("username")} type="text" placeholder="Username" required />
+            <input {...register("email")} type="email" placeholder="Email" required />
+            <input {...register("password")} type="password" placeholder="Password" required/>
+            <input type="submit" value="Submit" />
+        </form>
+        
+    );
+}
