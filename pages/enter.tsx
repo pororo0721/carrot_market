356423fb277cc00f3b3ac43188c61c167c161106
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-22 12:24:49 +0100

    code refactoring

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -13,16 +13,17 @@ interface EnterForm{
 
 const Enter: NextPage = () => {
   const [enter, {loading, data, error}] = useMutation("/api/users/enter");
-  const [submitting, setSubmitting] = useState(false);
+
   const {register, handleSubmit, reset} = useForm<EnterForm>()
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {reset(); setMethod("email")};
   const onPhoneClick = () => {reset(); setMethod("phone")};
 
-  const onValid = (data:EnterForm) =>{
-    enter(data);
+  const onValid = (validForm:EnterForm) =>{
+    if(loading) return;
+    enter(validForm);
   };
-  
+  console.log(loading, data, error);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -68,9 +69,9 @@ const Enter: NextPage = () => {
               required
             />
           ) : null}
-          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "email" ? <Button text={loading ? "Loading": "Get login link"} /> : null}
           {method === "phone" ? (
-            <Button text={submitting ? "Loading":"Get one-time password"} />
+            <Button text={loading? "Loading":"Get one-time password"} />
           ) : null}
         </form>
 
