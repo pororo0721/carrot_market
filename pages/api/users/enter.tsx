7e49b68b9cf6891aba5f8590c22b14f7e92fac83
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-27 13:03:00 +0100

    Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,7 +5,57 @@ import client from "../../../libs/server/client";
 
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const {phone, email} =req.body;
+  const payload = phone ? {phone: +phone} :{email}
+  const user = await client.user.upsert({
+    where:{
+      ...payload,
+    },
+    create:{
+      name:"Anonymous",
+      ...payload,
+    },
+    update:{},
+  });
+  console.log(user);
+
+  // if (email){
+  //   user = await client.user.findUnique({
+  //     where:{
+  //       email,
+  //     },
+  //   });
+  //   if(user) console.log("found it.")
+  //   if(!user){
+  //     console.log("Did not find. will create.")
+  //     user = await client.user.create({
+  //       data:{
+  //         name:"Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+
+  // if (phone){
+  //   user = await client.user.findUnique({
+  //     where:{
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if(phone) console.log("found it.")
+  //   if(!phone){
+  //     console.log("Did not find. will create.")
+  //     user = await client.user.create({
+  //       data:{
+  //         name:"Anonymous",
+  //         phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
   return res.status(200).end();
 }
 
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -23,7 +23,7 @@ const Enter: NextPage = () => {
     if(loading) return;
     enter(validForm);
   };
-  console.log(loading, data, error);
+  
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
