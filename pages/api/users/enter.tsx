115ff5b4c1a069958f19dc2a23551b29cba2403b
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-28 13:50:43 +0100

    Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
+
+mail.setApiKey(process.env.SENDGRID_APIKEY!);
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 
@@ -34,6 +37,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if(email) {
+    const email = await mail.send({
+      from: process.env.MY_EMAIL,
+      to: process.env.MY_EMAIL,
+      subject:"Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
 
   // if (email){
