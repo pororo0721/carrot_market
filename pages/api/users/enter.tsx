3e0d23f7a9ff874e1c62b0604aa4f99fdb6af1be
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-28 13:20:50 +0100

    Twilo Setup and Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,12 +1,15 @@
-import withHandler from "@libs/server/withHandler";
+import twilio from "twilio";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const {phone, email} =req.body;
-  const user = phone ? {phone: +phone} :{email}
+  const user = phone ? {phone: +phone} :email ? {email} :null;
+  if (!user) return res.status(400).json({ok: false});
   const payload = Math.floor(100000 + Math.random() * 900000) +"";
   const token = await client.token.create({
     data: {
@@ -24,7 +27,14 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
+  if (phone){
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
 
   // if (email){
   //   user = await client.user.findUnique({
@@ -63,7 +73,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   //   }
   //   console.log(user);
   // }
-  return res.status(200).end();
+  return res.json({
+    ok:true,
+
+  });
 }
 
 export default withHandler("POST", handler);
