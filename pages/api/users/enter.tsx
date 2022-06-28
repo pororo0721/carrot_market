3a720393784e933a36be108d0b53b4ad03b9f5e4
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-06-28 15:33:16 +0100

    Token UI

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -31,60 +31,24 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
     },
   });
   if (phone){
-    const message = await twilioClient.messages.create({
+ /*    const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
       to: process.env.MY_PHONE!,
       body: `Your login token is ${payload}`,
     });
-    console.log(message);
+    console.log(message); */
   } else if(email) {
-    const email = await mail.send({
+    /* const email = await mail.send({
       from: process.env.MY_EMAIL,
       to: process.env.MY_EMAIL,
       subject:"Your Carrot Market Verification Email",
       text: `Your token is ${payload}`,
       html: `<strong>Your token is ${payload}</strong>`,
     });
-    console.log(email);
+    console.log(email); */
   }
 
-  // if (email){
-  //   user = await client.user.findUnique({
-  //     where:{
-  //       email,
-  //     },
-  //   });
-  //   if(user) console.log("found it.")
-  //   if(!user){
-  //     console.log("Did not find. will create.")
-  //     user = await client.user.create({
-  //       data:{
-  //         name:"Anonymous",
-  //         email,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
 
-  // if (phone){
-  //   user = await client.user.findUnique({
-  //     where:{
-  //       phone: +phone,
-  //     },
-  //   });
-  //   if(phone) console.log("found it.")
-  //   if(!phone){
-  //     console.log("Did not find. will create.")
-  //     user = await client.user.create({
-  //       data:{
-  //         name:"Anonymous",
-  //         phone,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
   return res.json({
     ok:true,
 
