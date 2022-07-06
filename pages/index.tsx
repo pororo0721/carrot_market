1eba2d9548e9fdc6c00155c31ce357543568327c
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-07-06 11:45:29 +0100

    useUser Refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,16 @@ import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  const user = useUser();
+  const {user,isLoading} = useUser();
   console.log(user);
   return (
     <Layout title="홈" hasTabBar>
-      <title>Home</title>
+       <Head>
+        <title>Home</title>
+      </Head>
       <div className="flex flex-col space-y-5 divide-y">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <Item
