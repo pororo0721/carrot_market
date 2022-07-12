c7efb79b6af86d2b328f1d85f45157a56424eb7b
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-07-12 14:20:54 +0100

    bound mutations

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -20,11 +20,13 @@ interface ItemDetailResponse {
 
 const ItemDetail: NextPage = () => {
   const router = useRouter();
-  const { data } = useSWR<ItemDetailResponse>(
+  const { data, mutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavoriteClick = () => {
+    if (!data) return;
+    mutate({...data, isLiked: !data.isLiked}, false);
     toggleFav({});
   }
   return (
