befb8d582951d0bb4924219ee6551a034f65ebf0
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-07-25 12:51:30 +0100

    Geo Search in post

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -7,8 +7,8 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const {body:{ question, latitude, longitude}, session:{user}} =req;
   if (req.method === "POST") {
+    const {body:{ question, latitude, longitude}, session:{user}} =req;
     const post = await client.post.create({
       data: {
           question,
@@ -28,6 +28,10 @@ async function handler(
     });
   }
   if(req.method === "GET") {
+    const{query:{latitude,longitude}} =req;
+    const parsedLatitude= parseFloat(latitude.toString())
+    const parsedLongitue = parseFloat(longitude.toString())
+
     const posts = await client.post.findMany({
       include:{
         user:{
@@ -41,9 +45,19 @@ async function handler(
           select:{
             wondering:true,
             answers:true,
-          }
-        }
-      }
+          },
+        },
+      },
+      where: {
+        latitude: {
+          gte: parsedLatitude - 0.01,
+          lte: parsedLatitude + 0.01,
+        },
+        longitude: {
+          gte: parsedLongitue - 0.01,
+          lte: parsedLongitue + 0.01,
+        },
+      },
     });
     res.json ({
       ok:true,
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -4,6 +4,7 @@ import FloatingButton from "@components/floating-button";
 import Layout from "@components/layout";
 import useSWR from "swr";
 import { Post, User } from "@prisma/client";
+import useCoords from "@libs/client/useCoords";
 
 interface PostWithUser extends Post {
   user: User;
@@ -20,8 +21,8 @@ interface PostResponse {
 }
 
 const Community: NextPage = () => {
-  const { data } = useSWR<PostResponse>(`/api/posts`);
-  console.log(data);
+  const {latitude, longitude} = useCoords();
+  const { data } = useSWR<PostResponse>(`/api/posts?latitude=${latitude}&longitude=${longitude}`);
   return (
     <Layout hasTabBar title="동네생활">
       <div className="space-y-4 divide-y-[2px]">
