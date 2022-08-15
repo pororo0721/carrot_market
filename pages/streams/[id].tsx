d320ade317b008403d65119b3bc96f89ab26f584
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-08-15 15:21:25 +0100

    video and thumbnails

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -65,7 +65,14 @@ const Stream: NextPage = () => {
   return (
     <Layout canGoBack>
       <div className="py-10 px-4  space-y-4">
-        <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+      {jsonPage?.stream.cloudflareId ? (
+          <iframe
+            className="w-full aspect-video  rounded-md shadow-sm"
+            src={`https://iframe.videodelivery.net/${jsonPage?.stream.cloudflareId}`}
+            allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+            allowFullScreen={true}
+          ></iframe>
+        ) : null}
         <div className="mt-5">
           <h1 className="text-3xl font-bold text-gray-900">
           {jsonPage?.stream?.name}
