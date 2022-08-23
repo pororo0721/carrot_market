8c3b75d24ae3ee92d9af3b087dbce0cf924e1c20
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-08-23 13:25:14 +0100

    Incremental static Regeneration

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -5,6 +5,7 @@ import Layout from "@components/layout";
 import useSWR from "swr";
 import { Post, User } from "@prisma/client";
 import useCoords from "@libs/client/useCoords";
+import client from "@libs/server/client";
 
 interface PostWithUser extends Post {
   user: User;
@@ -16,11 +17,10 @@ interface PostWithUser extends Post {
 }
 
 interface PostResponse {
-  ok: boolean;
   posts: PostWithUser[]
 }
 
-const Community: NextPage = () => {
+const Community: NextPage<PostResponse> = ({posts}) => {
   const {latitude, longitude} = useCoords();
   const { data } = useSWR<PostResponse>(latitude && longitude ? `/api/posts?latitude=${latitude}&longitude=${longitude}` : null);
   return (
@@ -55,7 +55,7 @@ const Community: NextPage = () => {
                       d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                     ></path>
                   </svg>
-                  <span>궁금해요 {post._count.wondering}</span>
+                  <span>궁금해요 {post._count?.wondering}</span>
                 </span>
                 <span className="flex space-x-2 items-center text-sm">
                   <svg
@@ -72,7 +72,7 @@ const Community: NextPage = () => {
                       d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                     ></path>
                   </svg>
-                  <span>답변 {post._count.answers}</span>
+                  <span>답변 {post._count?.answers}</span>
                 </span>
               </div>
             </a>
@@ -98,5 +98,17 @@ const Community: NextPage = () => {
     </Layout>
   );
 };
+
+// Incremental Static Regeneration
+
+// export async function getStaticProps(){
+//   const posts = await client.post.findMany({include:{user:true}})
+//   return {
+//     props: {
+//       posts: JSON.parse(JSON.stringify(posts))
+//     },
+//     revalidate:10,
+//   }
+// }
 
 export default Community;
