0c21d555414bad687e2318db527af3918da27a2f
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-08-24 12:47:25 +0100

    Blocking SSG

--- _middleware.ts
+++ _middleware.ts
@@ -4,10 +4,11 @@ import { NextResponse } from "next/server";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     if(req.ua?.isBot) {
         return new Response("Plz don't be a bot. Be human.", {status: 403});
-    }
-    if(!req.url.includes("/api")){
-        if(!req.url.includes("/enter") && !req.cookies.Carrotsession){
-            return NextResponse.redirect(`${req.nextUrl.origin}/enter`)
-        };
+    } else if (req?.ua?.isBot !== undefined) {
+        if(!req.url.includes("/api")){
+            if(!req.url.includes("/enter") && !req.cookies.Carrotsession){
+                return NextResponse.redirect(`${req.nextUrl.origin}/enter`)
+            };
+        }
     }
 }
